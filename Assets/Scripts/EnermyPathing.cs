1058ba9b44a8268e0a6acd267d0c05109cbde8f1
velcronator <valacos7@gmail.com>
2020-11-04 12:16:05 +1100

    Move Enermy on path

--- Assets/Scripts/EnermyPathing.cs
+++ Assets/Scripts/EnermyPathing.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class EnermyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+    private Vector3 targetPosition;
+    private Vector3 currentPosition;
+    private float stepThisFrame;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveAlongWaypointPath();
+    }
+
+    private void MoveAlongWaypointPath()
+    {
+        // check to see if the last waypoint has been reached
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            // move towards target waypoint
+            targetPosition = waypoints[waypointIndex].transform.position;
+            stepThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, stepThisFrame);
+            // check to see if reached the waypoint
+            if (transform.position == targetPosition)
+            {   // increment waypoint
+                waypointIndex++;
+            }
+        }
+        else
+        {// if you reached the last waypoint
+            Destroy(gameObject);
+        }
     }
 }
