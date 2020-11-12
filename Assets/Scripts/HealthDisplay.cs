ff069fc7c6c37a863e5fa4a052c2e526fc547f4a
velcronator <valacos7@gmail.com>
2020-11-12 21:08:22 +1100

    Added Health Bar

--- Assets/Scripts/HealthDisplay.cs
+++ Assets/Scripts/HealthDisplay.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
 
     TMP_Text scoretext;
     Player player;
+    Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
         scoretext = GetComponent<TMP_Text>();
         player = FindObjectOfType<Player>();
+        slider = FindObjectOfType<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.LogError(player.GetHealth().ToString());
         scoretext.text = player.GetHealth().ToString();
+        slider.value = player.GetHealth();
     }
 }
